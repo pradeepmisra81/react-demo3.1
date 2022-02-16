ac54f196076b6134250a7b409534bfad9199b93b
pradeep <pradeep.misra@gmail.com>
2022-02-16 14:59:41 +0530

    added the form

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Card from '../UI/Card';
+
+
+const AddUser = (props) => {
+    const addUserHandler = (event) => {
+        event.preventDefault();
+    }
+
+  return (
+      <Card>
+    <form onSubmit={addUserHandler}>
+    <label htmlFor="username">Username</label>
+    <input id="username" type="text"></input>
+    <label htmlFor='age'>Age (Years)</label>
+    <input htmlFor="age" type="number"></input>
+    <button type="submit">Add User</button>
+  </form>
+  </Card>
+  );
+}
+
+export default AddUser;
