4747625938a88b9faa1920f2628d463acebc9796
pradeep <pradeep.misra@gmail.com>
2022-02-16 18:38:05 +0530

    added styling for adduser component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Card from '../UI/Card';
 
+import classes from './AddUser.module.css';
+
 
 const AddUser = (props) => {
     const addUserHandler = (event) => {
@@ -8,7 +10,7 @@ const AddUser = (props) => {
     }
 
   return (
-      <Card>
+      <Card className={classes.input}>
     <form onSubmit={addUserHandler}>
     <label htmlFor="username">Username</label>
     <input id="username" type="text"></input>
