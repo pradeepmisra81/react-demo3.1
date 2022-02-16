c8bbbf61a1892328951ccf8014f6b4d3cc48de87
pradeep <pradeep.misra@gmail.com>
2022-02-16 15:11:02 +0530

    Added style for Card wrapper

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import classes from './Card.module.css'
 
 
 const Card = (props) => {
     
   return (
-    <div>
+    <div className={classes.card}>
         {props.children}
     </div>
   );
